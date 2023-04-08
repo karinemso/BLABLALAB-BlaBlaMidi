bd7a629add10e6054ae97350925aa38f12ae7498
Karine <karinemsoliveira@gmail.com>
2023-04-08 00:30:09 -0300

    Dó oitava acima

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ blafabtn= document.querySelector('.tecla_fa')
 blasolbtn = document.querySelector('.tecla_sol')
 blalabtn = document.querySelector('.tecla_la')
 blasibtn = document.querySelector('.tecla_si')
+blado2btn = document.querySelector('.tecla_do2')
 blado = document.querySelector('#som_tecla_do')
 blare = document.querySelector('#som_tecla_re')
 blami = document.querySelector('#som_tecla_mi')
@@ -12,11 +13,11 @@ blafa= document.querySelector('#som_tecla_fa')
 blasol = document.querySelector('#som_tecla_sol')
 blala = document.querySelector('#som_tecla_la')
 blasi = document.querySelector('#som_tecla_si')
+blado2 = document.querySelector('#som_tecla_do2')
 
+teclas = [bladobtn,blarebtn,blamibtn,blafabtn,blasolbtn,blalabtn,blasibtn, blado2btn]
 
-teclas = [bladobtn,blarebtn,blamibtn,blafabtn,blasolbtn,blalabtn,blasibtn]
-
-sons = [blado,blare,blami,blafa,blasol,blala,blasi]
+sons = [blado,blare,blami,blafa,blasol,blala,blasi, blado2]
 
 function reproduza(i){
     console.log(i)
